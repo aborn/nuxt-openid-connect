1c108d8c9ed51e4b415f5ac3183e135401d54b30
aborn <loveaborn@foxmail.com>
2022-07-01 10:11:55 +0800

    add fetch demo

--- playground/pages/hello.vue
+++ playground/pages/hello.vue
@@ -13,14 +13,19 @@ const { $oidc } = useNuxtApp()
 const user = $oidc.user
 console.log(user)
 
+const uid = user ? user.sub : 'default'
+
 const url = 'http://localhost:18080/api/mindpress/demo'
 
-const { data: dataServer } = await useFetch(url,
-  {
-    key: 't' + new Date(),
-    headers: { uid: 'aborn' }
-  })
-console.log(dataServer.value)
+if (process.client) {
+  const { data: dataServer } = await useFetch(url,
+    {
+      key: 't' + new Date(),
+      headers: { uid }
+    })
+  console.log(dataServer)
+  console.log(dataServer.value)
+}
 
 </script>
 
@@ -30,12 +35,12 @@ export default {
     return {
     }
   },
-  rendered () {
+  rendered() {
     console.log('gggggg')
     const { $oidc } = useNuxtApp()
     console.log($oidc.user)
   },
-  mounted () {
+  mounted() {
     console.log('mounted...')
     const { $oidc } = useNuxtApp()
     console.log($oidc.user)
