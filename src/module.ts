4e0e0460b61007e7151faaa510c4eae2d3c24f91
aborn <loveaborn@foxmail.com>
2022-12-13 16:50:15 +0800

    version: 0.4.3

--- src/module.ts
+++ src/module.ts
@@ -1,8 +1,18 @@
 import { fileURLToPath } from 'url'
 import { defineNuxtModule, addPlugin, resolveModule, createResolver } from '@nuxt/kit'
 import defu from 'defu'
-import { CookieSerializeOptions } from 'cookie-es'
 import { name, version } from '../package.json'
+
+export type CookieSerializeOptions = {
+  domain?: string | undefined;
+  encode?(value: string): string;
+  expires?: Date | undefined;
+  httpOnly?: boolean | undefined;
+  maxAge?: number | undefined;
+  path?: string | undefined;
+  sameSite?: true | false | 'lax' | 'strict' | 'none' | undefined;
+  secure?: boolean | undefined;
+}
 
 export type OidcProvider = {
   issuer: string,
