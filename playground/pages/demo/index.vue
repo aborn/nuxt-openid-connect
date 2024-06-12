f16014a7c02d2dd32d583b68c1803d51542b2eb4
aborn <loveaborn@foxmail.com>
2024-06-12 22:26:41 +0800

    add demo page.

---
+++ playground/pages/demo/index.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <NavBar />
+    <main class="container">
+      <h1> Public Page </h1>
+      <b-alert show variant="success">
+        An demo page!
+      </b-alert>
+    </main>
+  </div>
+</template>
+
+<script lang="ts">
+</script>
