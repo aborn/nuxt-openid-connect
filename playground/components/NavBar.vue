43269e5026204f9331b2dae388c672629decdeb8
aborn <loveaborn@foxmail.com>
2024-06-12 22:55:56 +0800

    upgrade nuxt to latest

--- playground/components/NavBar.vue
+++ playground/components/NavBar.vue
@@ -1,37 +1,39 @@
 <template>
-  <nav class="container-fluid">
-    <ul>
-      <li>
-        <NuxtLink to="/" aria-label="Back home">
-          Nuxt-Openid-Connect
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/" class="first">
-          Home
-        </NuxtLink>
-      </li>
-    </ul>
-    <ul>
-      <li>
-        <NuxtLink to="/public" class="secondary">
-          Public
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink to="/secure" class="secondary">
-          Secure
-        </NuxtLink>
-      </li>
-      <li>
-        <NuxtLink v-if="$oidc.isLoggedIn" to="/secure" class="contrast" aria-label="My files">
-          <img :src="$oidc.user.avatar" style="height:50px;max-width:100%;border-radius: 50%;">
-          <span>{{ $oidc.user.name }}</span>
-        </NuxtLink>
-        <button v-else @click="$oidc.login()">
-          login
-        </button>
-      </li>
-    </ul>
-  </nav>
+  <div>
+    <nav class="container-fluid">
+      <ul>
+        <li>
+          <NuxtLink to="/" aria-label="Back home">
+            Nuxt-Openid-Connect
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/" class="first">
+            Home
+          </NuxtLink>
+        </li>
+      </ul>
+      <ul>
+        <li>
+          <NuxtLink to="/public" class="secondary">
+            Public
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink to="/secure" class="secondary">
+            Secure
+          </NuxtLink>
+        </li>
+        <li>
+          <NuxtLink v-if="$oidc.isLoggedIn" to="/secure" class="contrast" aria-label="My files">
+            <img :src="$oidc.user.avatar" style="height:50px;max-width:100%;border-radius: 50%;">
+            <span>{{ $oidc.user.name }}</span>
+          </NuxtLink>
+          <button v-else @click="$oidc.login()">
+            login
+          </button>
+        </li>
+      </ul>
+    </nav>
+  </div>
 </template>
